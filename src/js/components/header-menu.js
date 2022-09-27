8e0ced70dff3ab29796327145a96e973ddd60996
Oleksandr Kryvoruchko <oleksandrkryvoruchko2000@gmail.com>
2022-09-28 00:25:52 +0300

    first version of header

--- src/js/components/header-menu.js
+++ src/js/components/header-menu.js
@@ -1,28 +1,29 @@
-const headerMenuBtn = document.querySelector('.header__menu-btn')
+const headerMenuBtn = document.querySelector('.header__btn-open')
+const headerMenuClose = document.querySelector('.header__btn-close')
 const headerMenuWrapper = document.querySelector('.header__menu-wrapper')
-const headerMenu = document.querySelector('.header-menu')
+const headerMenuOverlay = document.querySelector('.header__menu-overlay')
 
-function closeHeaderMenu(e) {
-  if ((!e.target.closest('.header-menu') && !e.target.closest('.header__menu-btn'))
-    || e.target.closest('.header-menu__link')) {
-    headerMenuWrapper.classList.remove('header__menu-wrapper--active')
-    document.removeEventListener('click', closeHeaderMenu)
-    headerMenu.style.maxHeight = null
-    document.body.classList.remove('no-scroll')
-  }
-}
+// const headerMenu = document.querySelector('.header-menu')
+
+// function closeHeaderMenu(e) {
+//   if ((!e.target.closest('.header-menu') && !e.target.closest('.header__menu-btn'))
+//     || e.target.closest('.header-menu__link')) {
+//     headerMenuWrapper.classList.remove('header__menu-wrapper--active')
+//     document.removeEventListener('click', closeHeaderMenu)
+//     headerMenu.style.maxWidth = null
+//     document.body.classList.remove('no-scroll')
+//   }
+// }
 
 headerMenuBtn.addEventListener('click', () => {
   headerMenuWrapper.classList.toggle('header__menu-wrapper--active')
+})
 
-  if (headerMenu.style.maxHeight) {
-    headerMenu.style.maxHeight = null
-    document.removeEventListener('click', closeHeaderMenu)
-    document.body.classList.remove('no-scroll')
-  }
-  else {
-    headerMenu.style.maxHeight = `${headerMenu.scrollHeight}px`
-    document.addEventListener('click', closeHeaderMenu)
-    document.body.classList.add('no-scroll')
-  }
+headerMenuClose.addEventListener('click', () => {
+  headerMenuWrapper.classList.toggle('header__menu-wrapper--active')
+})
+
+headerMenuOverlay.addEventListener('click', (e) => {
+  if (e.target === headerMenuOverlay)
+    headerMenuWrapper.classList.toggle('header__menu-wrapper--active')
 })
