1317daece8183132f8df2f0813b64be889260088
Oleksandr Kryvoruchko <oleksandrkryvoruchko2000@gmail.com>
2022-12-23 22:33:48 +0200

    reviews section

---
+++ src/js/components/initializeReviewsSlider.js
@@ -0,0 +1,37 @@
+import Swiper, { Autoplay, Keyboard, Pagination } from 'swiper'
+import 'swiper/css'
+
+function initializeReviewsSlider() {
+  const swiper = new Swiper('.swiper', {
+    modules: [Autoplay, Keyboard, Pagination],
+    loop: true,
+    keyboard: true,
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: true,
+    // },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      540: {
+        spaceBetween: 30,
+      },
+      768: {
+        spaceBetween: 30,
+
+      },
+      1024: {
+        spaceBetween: 50,
+
+      },
+    },
+
+  })
+}
+
+export { initializeReviewsSlider }
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,3 @@
+import { initializeReviewsSlider } from './components/initializeReviewsSlider'
 import './components/header-menu'
+initializeReviewsSlider()
